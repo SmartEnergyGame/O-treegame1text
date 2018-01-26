f2cebb98b9f0f52d71426ddefe7b2705cb1f46f1
Aldana-Sigona, Jesus-Enrique <jealdana@users.noreply.github.com>
2018-01-26 11:38:53 -0500

    indentation issue

--- survey/models.py
+++ survey/models.py
@@ -19,8 +19,8 @@ class Subsession(BaseSubsession):
     def creating_session(self):
           treatments = itertools.cycle(['control', 'D', 'DTI'])
           if self.round_number == 1:
-          	 self.group_randomly()
-             for g in self.get_groups():
+            self.group_randomly()
+            for g in self.get_groups():
                  treat = next(treatments)
                  for p in g.get_players():
                      p.participant.vars['treatment'] = treat
