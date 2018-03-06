c5d22a7a9ea7ed7ecd2d8fbe1c55324d99a9e2b4
Aldana-Sigona, Jesus-Enrique <jealdana@users.noreply.github.com>
2018-03-05 20:16:11 -0500

    2 columns implementation

--- public_goods/views.py
+++ public_goods/views.py
@@ -44,7 +44,7 @@ class Contribute(Page):
 class ResultsWaitPage(WaitPage):
     def after_all_players_arrive(self):
         self.group.set_payoffs()
-    body_text = "Waiting for other participants to contribute."
+    body_text = "Please wait for the other participants to respond."
 
 class Results_c(Page):
     def vars_for_template(self):
@@ -77,16 +77,12 @@ class Results_D(Page):
             parts_savings = [p.savings.to_real_world_currency(self.session) for p in
                      treatment_group]
             parts_labels = ["participant "+str(label) for label in range(len(treatment_group))]
-            if self.player.savings >= average_savings:
-                position = ''' You are above average'''
-            else:
-                position = ''' You are below average'''
             return {
                 'endowment': self.player.participant.vars['endowment'].to_real_world_currency(self.session),
                 'savings': self.player.savings.to_real_world_currency(self.session),
                 'last_savings': self.player.last_savings.to_real_world_currency(self.session),
                 'total_savings': cum_earnings,
-                'position': position,
+
                 'parts': parts,
                 'parts_savings': parts_savings,
                 'my_total_savings': sum([p.savings for p in self.player.in_all_rounds()]),
@@ -107,16 +103,10 @@ class Results_DTI(Page):
             [p.savings for p in self.group.in_round(round_id).get_players() if p.treatment == self.player.treatment])
                              for round_id in
                              range(1, self.player.round_number + 1)])).to_real_world_currency(self.session)
-        parts = [{'id_in_group':  p.participant.id_in_session, 'savings': p.savings.to_real_world_currency(self.session)} for p in
+        parts = [{'id_in_group':  p.participant.id_in_session, 'savings': p.savings.to_real_world_currency(self.session)
+                  ,'ind_cum_savings':(sum([p.savings for p in p.in_all_rounds()])).to_real_world_currency(self.session)} for p in
                  treatment_group]
-        average_savings = sum([p.savings for p in treatment_group]) / len(treatment_group)
-        parts_savings = [p.savings.to_real_world_currency(self.session) for p in
-                         treatment_group]
-        parts_labels = ["participant " + str(label) for label in range(len(treatment_group))]
-        if self.player.savings >= average_savings:
-            position = ''' You are above average'''
-        else:
-            position = ''' You are below average'''
+
         if self.player.participant.vars['role'] == 'type1':
             injunctive_label = "Remember, contributing to the collective energy conservation goal is important to to help reduce air and water pollution creating health problems affecting you and your family and improve your chances of gaining a larger share of the conservation account funds."
         else:
@@ -128,12 +118,11 @@ class Results_DTI(Page):
             'savings': self.player.savings.to_real_world_currency(self.session),
             'last_savings': self.player.last_savings.to_real_world_currency(self.session),
             'total_savings': cum_earnings,
-            'position': position,
             'parts': parts,
-                'parts_savings': parts_savings,
-                'parts_label': parts_labels,
             'injunctive_label': injunctive_label,
             'my_total_savings': (sum([p.savings for p in self.player.in_all_rounds()])).to_real_world_currency(self.session),
+            'current_total_savings': (sum([p.savings for p in self.player.group.get_players()])).to_real_world_currency(
+                self.session),
             'goal':c(Constants.num_rounds*len([p.savings for p in self.group.in_round(1).get_players()])*.5).to_real_world_currency(self.session),
         }
 
