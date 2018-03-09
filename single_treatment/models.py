955a5b48438f4a7d78c61f1efbb41fda4c1e340e
Aldana-Sigona, Jesus-Enrique <jealdana@users.noreply.github.com>
2018-03-09 00:20:52 -0600

    typos

--- single_treatment/models.py
+++ single_treatment/models.py
@@ -37,12 +37,12 @@ class Player(BasePlayer):
     #choices1q = [c(0).to_real_world_currency(BasePlayer.session), c(1*2), c(1*4)]
     def check_answersP2(self):
         correct_answer = "Correct!"
-        if self.q4 == "$ 26":
+        if self.q4 == "$26.00":
             q4_feedback = [1,"Your answer was: "+ self.q4 , correct_answer]
 
         else:
             q4_feedback = [0,"Your answer was: "+ self.q4 , "In this scenario, the group has met the threshold for the incentive from the conservation account. This means you get an equal share of the $ 90 group incentive ($ 30 x 3), or $15. In addition, you keep the $ 11 remaining in your private account. Your total payout is $ 15 + $ 11 = $ 26"]
-        if self.q5 == "$ 11":
+        if self.q5 == "$11.00":
             q5_feedback = [1,"Your answer was: "+ self.q5 , correct_answer]
 
         else:
@@ -59,17 +59,17 @@ class Player(BasePlayer):
     def check_answers(self):
         correct_answer = "Correct!"
 
-        if self.q1 == "$ 0.5":
+        if self.q1 == "$0.50":
             q1_feedback = [1,"Your answer was: "+ self.q1 , correct_answer]
 
         else:
-            q1_feedback = [0,"Your answer was: "+ self.q1 , "The energy conservation goal is $ " + str(8*.5*(len(self.get_others_in_subsession())+1)) + ", so each player has to contribute $ 0.50 each round, on average, to meet the goal."]
+            q1_feedback = [0,"Your answer was: "+ self.q1 , "The energy conservation goal is $ " + str(8*.5*(len(self.get_others_in_subsession())+1)) + ", so each player has to contribute $0.50 each round, on average, to meet the goal."]
         if self.q2 == 'Three times the total in the conservation account ':
             q2_feedback = [1,"Your answer was: "+ self.q2 , correct_answer]
 
         else:
             q2_feedback = [0,"Your answer was: "+ self.q2 , "The total incentive payment to the group is equal to three times the total contributions in the conservation account at the end of the game."]
-        if self.q3 == "$ 12":
+        if self.q3 == "$12.00":
             q3_feedback = [1,"Your answer was: "+ self.q3 , correct_answer]
 
         else:
@@ -92,7 +92,7 @@ class Player(BasePlayer):
     q1 = models.CharField(
         doc="Question 1", 
         label="1. How much does each player have to invest into the conservation account, on average, for each round to meet the goal?",
-        choices = ["$ 0", "$ 0.5", "$ 1.0"],
+        choices = ["$0.00", "$0.50", "$1.00"],
         widget=widgets.RadioSelectHorizontal,
     )
     q2 = models.CharField(
@@ -107,33 +107,33 @@ class Player(BasePlayer):
          )
     q3 = models.CharField(
         doc="Question 3", 
-        label="3. Assume the 6 players in the game have contributed $24 to the conservation account, meeting the account goal. How much will each player receive as a bonus for meeting the conservation account goal?  Each player will receive $ ",
+        label="3. Assume the 6 players in the game have contributed $24.00 to the conservation account, meeting the account goal. How much will each player receive as a bonus for meeting the conservation account goal?  Each player will receive $ ",
         choices=[
-            "$ 11",
-            "$ 12",
-            "$ 24"
+            "$11.00",
+            "$12.00",
+            "$24.00"
         ],widget=widgets.RadioSelectHorizontal,
 
     )
     
     q4 = models.CharField(
         doc="Question 4",
-        label="4. Assume there are 6 players in the game and they have contributed $30 to the conservation account by the end of the experiment, and you have personally invested a total of $5 over the 8 rounds, leaving $11 in your private account. How much money will you receive at the end of the experiment, after the conservation account bonus incentive has been paid? My payout is $"
+        label="4. Assume there are 6 players in the game and they have contributed $30.00 to the conservation account by the end of the experiment, and you have personally invested a total of $5.00 over the 8 rounds, leaving $11.00 in your private account. How much money will you receive at the end of the experiment, after the conservation account bonus incentive has been paid? My payout is $"
         , choices=[
-            "$ 15",
-            "$ 11",
-            "$ 26"
+            "$15.00",
+            "$11.00",
+            "$26.00"
         ],widget=widgets.RadioSelectHorizontal,
     )
 
     
     q5 = models.CharField(
         doc="Question 5",
-        label="5. Assume that the goal of $24 has NOT been contributed to the conservation account and you have invested a total of $5 in the 8 rounds, leaving $11 in your private account. How much money will you receive at the end of the experiment.  My payout is $"
+        label="5. Assume that the goal of $24.00 has NOT been contributed to the conservation account and you have invested a total of $5.00 in the 8 rounds, leaving $11.00 in your private account. How much money will you receive at the end of the experiment.  My payout is $"
         , choices=[
-            "$ 5",
-            "$ 11",
-            "$ 26"
+            "$5.00",
+            "$11.00",
+            "$26.00"
         ],widget=widgets.RadioSelectHorizontal,
     )
     
